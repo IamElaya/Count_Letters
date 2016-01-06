c412e731761657fc5055de541bcd56d63dda2f3d
IamElaya <asison13@gmail.com>
2016-01-06 15:02:34 -0800

    Update count_letters.rb

--- count_letters.rb
+++ count_letters.rb
@@ -1,5 +1,10 @@
 def count_letters(string)
-  string.scan(/\w/).inject(Hash.new(0)){|word, counts| word[counts] += 1; word}
+  h = Hash.new(0)
+  string.each_char do |char|
+    next unless char =~/\w/
+    h[char] += 1
+  end
+  h
 end
 
 puts count_letters("yoyoyoyoyyoyoyoyo eeyyyy lmaozedong rofl lol")
